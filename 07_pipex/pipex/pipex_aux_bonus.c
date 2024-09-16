a3574730cb1be32157ceae34f1e97dd0c972c014
Htravanca <henriquetravanca@gmail.com>
2024-09-16 21:51:42 +0100

    asdasd

---
+++ 07_pipex/pipex/pipex_aux_bonus.c
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_aux.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hepereir <hepereir@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 16:43:47 by hepereir          #+#    #+#             */
+/*   Updated: 2024/09/15 16:44:46 by hepereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	ft_free(char **var)
+{
+	int	i;
+
+	i = 0;
+	if (!var || !*var)
+		return ;
+	while (var[i] != NULL)
+	{
+		free(var[i]);
+		i++;
+	}
+	free(var);
+}
+
+static char	*ft_find_path(char **options, char *cmd)
+{
+	int		i;
+	char	*temp;
+	char	*temp2;
+	char	*pfinal;
+
+	i = 0;
+	pfinal = NULL;
+	while (options[i])
+	{
+		temp = ft_strjoin(options[i], "/");
+		temp2 = ft_strjoin(temp, cmd);
+		free(temp);
+		if (access(temp2, F_OK) == 0)
+		{
+			pfinal = ft_strdup(temp2);
+			free(temp2);
+			break ;
+		}
+		free(temp2);
+		i++;
+	}
+	return (pfinal);
+}
+
+char	*ft_path(char **cmdsarr, char **envp)
+{
+	char	**options;
+	char	*pfinal;
+	int		i;
+
+	i = 0;
+	pfinal = NULL;
+	while (!ft_strnstr(envp[i], "PATH", 4)) // procura nos sistem variables a variavel PATH
+		i++;
+	options = ft_split(envp[i] + 5, ':'); // separa os paths possvies onde pode existir o cmd
+	if (!options)
+		return (NULL);
+	pfinal = ft_find_path(options, cmdsarr[0]); // tirar o "-l" do "ls -l"
+	ft_free(options);
+	return (pfinal);
+}
+
+int	ft_handle_error(int val, const char *msg)
+{
+	if (val < 0)
+	{
+		perror(msg);
+		exit(1);
+	}
+	return (val);
+}
+
+void	ft_split_result(char **cmdsarr)
+{
+	if (!cmdsarr || !cmdsarr[0])
+	{
+		perror("Error while spliting the comands");
+		exit(1);
+	}
+}
---
+++ 07_pipex/pipex/pipex_bonus.c
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hepereir <hepereir@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/15 16:43:40 by hepereir          #+#    #+#             */
+/*   Updated: 2024/09/15 16:43:41 by hepereir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	ft_execute_child1(int *fd, char **argv, char **envp)
+{
+	int		rfd;
+	char	*path;
+	char	**cmdsarr;
+
+	rfd = ft_handle_error(open(argv[1], O_RDONLY, 0777), "Error opening file1");
+	dup2(rfd, STDIN_FILENO);
+	dup2(fd[1], STDOUT_FILENO);
+	close(rfd);
+	close(fd[0]);
+	close(fd[1]);
+	cmdsarr = ft_split(argv[2], ' ');
+	ft_split_result(cmdsarr);
+	path = ft_path(cmdsarr, envp);
+	if (!path)
+	{
+		perror("Command not found");
+		exit(1);
+	}
+	execve(path, cmdsarr, envp);
+	perror("Error executing the cmd1");
+	ft_free(cmdsarr);
+	free(path);
+	exit(1);
+}
+
+void	ft_execute_child2(int *fd, char **argv, char **envp)
+{
+	int		wfd;
+	char	*path;
+	char	**cmdsarr;
+
+	wfd = ft_handle_error(open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0777),
+			"Error opening file2");
+	dup2(wfd, STDOUT_FILENO);
+	dup2(fd[0], STDIN_FILENO);
+	close(wfd);
+	close(fd[0]);
+	close(fd[1]);
+	cmdsarr = ft_split(argv[3], ' ');
+	ft_split_result(cmdsarr);
+	path = ft_path(cmdsarr, envp);
+	if (!path)
+	{
+		perror("Command not found");
+		exit(1);
+	}
+	execve(path, cmdsarr, envp);
+	perror("Error executing the cmd2");
+	ft_free(cmdsarr);
+	free(path);
+	exit(1);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	int	fd[2];
+	int	pid1;
+	int	pid2;
+
+	if (argc == 5)
+	{
+		ft_handle_error(pipe(fd), "Pipe error");
+		pid1 = ft_handle_error(fork(), "Fork error");
+		if (pid1 == 0)
+			ft_execute_child1(fd, argv, envp);
+		pid2 = ft_handle_error(fork(), "Fork error");
+		if (pid2 == 0)
+			ft_execute_child2(fd, argv, envp);
+		close(fd[0]);
+		close(fd[1]);
+		waitpid(pid1, NULL, 0);
+		waitpid(pid2, NULL, 0);
+	}
+	else
+		perror("Error in ARGS, correct usage: ./pipex file1 cmd1 cmd2 file2");
+	return (0);
+}
