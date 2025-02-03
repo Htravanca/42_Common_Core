4945e0bff448998e92a96460e217da5089909e78
Htravanca <henriquetravanca@gmail.com>
2025-02-03 23:05:51 +0000

    first parsing

--- 10_philo/philo/philo.c
+++ 10_philo/philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: hepereir <hepereir@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 17:35:32 by hepereir          #+#    #+#             */
-/*   Updated: 2025/02/03 21:48:02 by hepereir         ###   ########.fr       */
+/*   Updated: 2025/02/03 22:11:07 by hepereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,4 @@ int	main(int argc, char **argv)
         return (1);
     print_data();
     return (0);
-}
+} 
